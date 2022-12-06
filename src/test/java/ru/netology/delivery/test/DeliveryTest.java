7101a279881e861b122456a3be6098f4e622e4ac
Aleksey Beketov <alex311271@gmail.com>
2022-12-06 13:30:13 +0300

    initional commit

---
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -0,0 +1,56 @@
+package ru.netology.delivery.test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import ru.netology.delivery.data.DataGenerator;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
+class DeliveryTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+
+    @DisplayName("Should successful plan and replan meeting")
+    void shouldSuccessfulPlanAndReplanMeeting() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
+        $("[data-test-id=date] input").setValue(firstMeetingDate);
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $x("//span[contains(text(),'Запланировать')]").click();
+        $("[data-test-id=success-notification").shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Успешно! Встреча успешно запланирована на  " + firstMeetingDate),
+                        Duration.ofSeconds(15));
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
+        $("[data-test-id=date] input").setValue(secondMeetingDate);
+        $x("//span[contains(text(),'Запланировать')]").click();
+        $("[data-test-id=replan-notification").shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Необходимо подтверждение У вас уже запланирована встреча на другую дату. Перепланировать?"),
+                        Duration.ofSeconds(15));
+        $x("//span[contains(text(),'Перепланировать')]").click();
+        $("[data-test-id=success-notification]").shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Успешно! Встреча успешно запланирована на " + secondMeetingDate),
+                        Duration.ofSeconds(15));
+
+    }
+
+
+}
